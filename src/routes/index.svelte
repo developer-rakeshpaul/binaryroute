a55cef644e53eededdb3ddb7ee761ae22521aede
Rakesh Paul <rakesh@Rakeshs-MacBook-Pro.local>
2022-03-02 11:20:50 +0530

    Urql client init and Index page init

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { initClient, operationStore, query } from '@urql/svelte';
+
+	initClient({
+		url: import.meta.env.VITE_GRAPHQL_API
+	});
+
+	const projects = operationStore(`
+			query GetProjects {
+        projects {
+          name
+          slug
+          description
+          demo
+          sourceCode
+          image {
+            url
+          }
+        }
+      } 
+  `);
+
+	query(projects);
+</script>
+
+{#if $projects.fetching}
+	<p>Loading...</p>
+{:else if $projects.error}
+	<p>Oh no... {$projects.error.message}</p>
+{:else}
+	<ul>
+		{#each $projects.data.projects as project}
+			<li>{project.name}</li>
+		{/each}
+	</ul>
+{/if}
